f075f0ba807b3339892692b5aa64bd329434ee59
Jose <turumbo2010@gmail.com>
2016-08-20 14:00:24 -0300

    Files added

---
+++ res/script/script.js
@@ -0,0 +1,68 @@
+var req = new XMLHttpRequest;
+var reqElev = new XMLHttpRequest;
+
+function toCelsius(f) {
+    return f - 273.15;
+}
+
+function converterTimestamp(UNIX_timestamp){
+	var date = new Date(UNIX_timestamp*1000);
+	var hours = date.getHours();
+	var minutes = "0" + date.getMinutes();
+	var seconds = "0" + date.getSeconds();
+	return(hours + ':' + minutes.substr(-2));
+}
+
+function initialize(lat, lon) {
+	var mapProp = {center: new google.maps.LatLng(lat, lon), zoom: 11, mapTypeId: google.maps.MapTypeId.HYBRID};
+	var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
+}
+
+function buscar(){
+	var cidade = document.getElementById("nc-field").value;
+	document.getElementById("gif").style.display = "block";
+
+	req.open('GET', 'http://api.openweathermap.org/data/2.5/weather?q=' + cidade + '&appid=8a647d241d321d29784d0425ec44e1d2');
+	req.send(null);
+}
+
+function carregarInformacoes() {
+	var resp = req.responseText;
+	var resp_obj = JSON.parse(resp);
+
+	document.getElementById("nomeCidade").innerHTML = resp_obj['name'] + ', ' + resp_obj['sys']['country'];
+	document.getElementById("tempValor").innerHTML = toCelsius(resp_obj['main']['temp']).toFixed(2) + ' ºC';
+	document.getElementById("tempImg").src = "http://openweathermap.org/img/w/" + resp_obj['weather']['0']['icon'] + ".png";
+
+	tabela = document.getElementById("tabela");
+	tabela.rows[0].cells[1].innerHTML = toCelsius(resp_obj['main']['temp_max']).toFixed(2) + ' ºC';
+	tabela.rows[1].cells[1].innerHTML = toCelsius(resp_obj['main']['temp_min']).toFixed(2) + ' ºC';
+	tabela.rows[2].cells[1].innerHTML = converterTimestamp(resp_obj['sys']['sunrise']);
+	tabela.rows[3].cells[1].innerHTML = converterTimestamp(resp_obj['sys']['sunset']);
+	var lat = tabela.rows[4].cells[1].innerHTML = resp_obj['coord']['lat'];
+	var lon =tabela.rows[5].cells[1].innerHTML = resp_obj['coord']['lon'];
+
+	reqElev.open('GET', 'https://maps.googleapis.com/maps/api/elevation/jsonp?locations=' + lat + ',' + lon + '&key=AIzaSyCOCnj2CFR3wdYBHzi2paDyR7bZkj9gsLY');
+	reqElev.send(null);
+
+	google.maps.event.addDomListener(window, 'load', initialize(lat, lon));
+
+	var form = document.getElementById("formulario");
+	form.style.marginTop = "40px";
+	form.style.backgroundColor = "rgba(225, 225, 225, 0)";	
+	form.style.height = "auto";
+	document.getElementById("gif").style.display = "none";
+	document.getElementById("artigo").style.opacity = "1";
+}
+
+function obterAltitude(){
+	var resp = reqElev.responseText;
+	var resp_obj = JSON.parse(resp);
+	alert('ue');
+	tabela.rows[6].cells[1].innerHTML = resp_obj['results']['elevation'].toFixed(1);
+}
+
+req.onloadend = carregarInformacoes;
+reqElev.onloadend = obterAltitude;
+
+
